c67a28ac31dc26886f6987e0d2e8f1ae9651062a
wztbxl <35267233+wztbxl@users.noreply.github.com>
2024-01-25 17:33:28 +0800

    check the job running

--- jobcheck.sh
+++ jobcheck.sh
@@ -3,19 +3,17 @@
 # 循环执行脚本
 while true
 do
-    # 运行查询作业状态的脚本，并将结果存储到临时文件中
-    /star/u/wangzhen/bin/mysubmit | grep -E 'rcas[0-9]+ ' > temp.txt
-
-    # 解析临时文件的内容，提取每个节点的运行作业数量
+    # 运行查询作业状态的脚本，并将结果通过管道传递给grep命令进行过滤
+    /star/u/wangzhen/bin/mysubmit | grep -E 'rcas[0-9]+ ' |
+    # 解析管道传递过来的结果，提取每个节点的运行作业数量
     while IFS="| " read -r node jobs completed removed idle running held suspended
     do
-        echo "$node $jobs $completed $removed $idle $running $held $suspended"
         # 检查节点的运行作业数量
         if [[ "$running" =~ ^[0-9]+$ ]] && [ "$running" -gt 0 ]; then
-            # 将节点的信息写入out.txt文件
-            echo "$node $jobs $completed $removed $idle $running $held $suspended" >> out.txt
+            # 将节点的信息输出到标准输出
+            echo "$node $jobs $completed $removed $idle $running $held $suspended"
         fi
-    done < temp.txt
+    done
 
     # 每隔一分钟执行一次
     sleep 60
