7143017ef48f402e3829ca869b83506b91d96bf4
wztbxl <35267233+wztbxl@users.noreply.github.com>
2024-02-01 17:19:43 +0800

    read Xiaoxuan's file

--- Draw/genevents.h
+++ Draw/genevents.h
@@ -183,8 +183,8 @@ genevents::genevents(TTree *tree) : fChain(0)
 {
 // if parameter tree is not specified (or zero), connect the file
 // used to generate this class and read the Tree.
-  //char* dir="/star/u/xchu/pwg/DiPion0Corr/Pythia/Broadening/output/1";
-  char* dir="/star/u/wangzhen/data01/QA/ColdQCD/DiHardon_Simulation/output/1/";
+  char* dir="/star/u/xchu/pwg/DiPion0Corr/Pythia/Broadening/output/1";
+//   char* dir="/star/u/wangzhen/data01/QA/ColdQCD/DiHardon_Simulation/output/1/";
   TSystemDirectory DIR(dir, dir);
   TList *files = DIR.GetListOfFiles();
   TChain* trees = new TChain();
